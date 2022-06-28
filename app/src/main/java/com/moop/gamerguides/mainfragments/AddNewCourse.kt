51b6eafb2a7ae6da1a0c17835c321cd07c4ab8c4
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-06-28 20:17:36 +0700

    Daffa Haidar - Fix Bugs

--- app/src/main/java/com/moop/gamerguides/mainfragments/AddNewCourse.kt
+++ app/src/main/java/com/moop/gamerguides/mainfragments/AddNewCourse.kt
@@ -13,32 +13,12 @@ import com.moop.gamerguides.adapter.model.Course
 
 class AddNewCourse : Fragment() {
 
-    val courseTitleInput: EditText
-    get() = requireView().findViewById(R.id.course_title_input);
-
-    val courseDescriptionInput: EditText
-    get() = requireView().findViewById(R.id.course_description_input);
-
-    val category: Spinner
-    get() = requireView().findViewById(R.id.course_game_category_input);
-
-    val addCourseButton: Button
-    get() = requireView().findViewById(R.id.add_course_button);
-
-    val database = FirebaseDatabase.getInstance();
-    val myRef = database.getReference("courses");
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-        val categorySpinner = arrayOf("Action", "Adventure", "Casual", "Indie", "Massively Multiplayer", "Racing", "RPG", "Simulation", "Sports", "Strategy")
-        val categoryAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, categorySpinner)
-        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        category.adapter = categoryAdapter
-
-        return inflater.inflate(R.layout.fragment_add_new_course, container, false)
-
-
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_courses, container, false)
     }
 
 }
