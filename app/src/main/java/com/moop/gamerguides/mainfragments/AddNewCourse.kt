ff046c1251411375cf7a6a8334ba30d657b747dc
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-06-28 17:31:52 +0700

    Daffa Haidar - Try to Implement Add Course

--- app/src/main/java/com/moop/gamerguides/mainfragments/AddNewCourse.kt
+++ app/src/main/java/com/moop/gamerguides/mainfragments/AddNewCourse.kt
@@ -5,15 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.moop.gamerguides.R
+import com.moop.gamerguides.adapter.model.Course
 
 class AddNewCourse : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    val courseTitleInput: EditText
+    get() = requireView().findViewById(R.id.course_title_input);
+
+    val courseDescriptionInput: EditText
+    get() = requireView().findViewById(R.id.course_description_input);
+
+    val category: Spinner
+    get() = requireView().findViewById(R.id.course_game_category_input);
+
+    val addCourseButton: Button
+    get() = requireView().findViewById(R.id.add_course_button);
+
+    val database = FirebaseDatabase.getInstance();
+    val myRef = database.getReference("courses");
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val categorySpinner = arrayOf("Action", "Adventure", "Casual", "Indie", "Massively Multiplayer", "Racing", "RPG", "Simulation", "Sports", "Strategy")
+        val categoryAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, categorySpinner)
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        category.adapter = categoryAdapter
+
         return inflater.inflate(R.layout.fragment_add_new_course, container, false)
+
+
+
     }
+
 }
