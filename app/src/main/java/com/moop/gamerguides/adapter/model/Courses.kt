8ee6bac54bafe5fa2974af848677f390cb898e28
alberterc <85069947+alberterc@users.noreply.github.com>
2022-06-29 18:36:06 +0700

    Changed how data is saved in firebase database.

--- app/src/main/java/com/moop/gamerguides/adapter/model/Courses.kt
+++ app/src/main/java/com/moop/gamerguides/adapter/model/Courses.kt
@@ -6,13 +6,15 @@ class Courses {
     var description: String? = ""
     var gameCategory: String? = ""
     var uid: String? = ""
+    var id: String? = ""
 
-    constructor(title: String, image: String, description: String, gameCategory: String, uid: String) {
+    constructor(title: String, image: String, description: String, gameCategory: String, uid: String, id: String) {
         this.title = title
         this.image = image
         this.description = description
         this.gameCategory = gameCategory
         this.uid = uid
+        this.id = id
     }
 
     constructor() {} // needed for firebase
