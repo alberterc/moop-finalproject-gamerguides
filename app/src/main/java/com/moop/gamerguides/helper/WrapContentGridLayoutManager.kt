e1b02b000974506d1c929b08b672182c94a629fa
alberterc <85069947+alberterc@users.noreply.github.com>
2022-06-30 03:06:35 +0700

    Removed WrapContentLinearLayoutManager

--- app/src/main/java/com/moop/gamerguides/helper/WrapContentGridLayoutManager.kt
+++ app/src/main/java/com/moop/gamerguides/helper/WrapContentGridLayoutManager.kt
@@ -3,27 +3,13 @@ package com.moop.gamerguides.helper
 import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-// util code from https://stackoverflow.com/questions/69516689/inconsistency-detected-invalid-view-holder-adapter-error
 // fixes recyclerview content inconsistency when getting updates from firebase database
-class WrapContentLinearLayoutManager : LinearLayoutManager {
-    constructor(context: Context?) : super(context) {}
-    constructor(context: Context?, orientation: Int, reverseLayout: Boolean) : super(
-        context,
-        orientation,
-        reverseLayout
-    ) {
-    }
-
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(
-        context,
-        attrs,
-        defStyleAttr,
-        defStyleRes
-    ) {
-    }
+class WrapContentGridLayoutManager : GridLayoutManager {
+    constructor(context: Context?, spanCount: Int) : super(context, spanCount) {}
 
     override fun onLayoutChildren(recycler: RecyclerView.Recycler, state: RecyclerView.State) {
         try {
