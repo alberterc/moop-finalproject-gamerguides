311ed16d137871e0a86192744eb627c5e327aae7
alberterc <85069947+alberterc@users.noreply.github.com>
2022-06-30 04:18:38 +0700

    Added video list function.

--- app/src/main/java/com/moop/gamerguides/CourseDetails.kt
+++ app/src/main/java/com/moop/gamerguides/CourseDetails.kt
@@ -1,27 +1,35 @@
+@file:Suppress("DEPRECATION")
+
 package com.moop.gamerguides
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.text.method.ScrollingMovementMethod
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.android.material.imageview.ShapeableImageView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
+import com.moop.gamerguides.adapter.VideoAdapter
+import com.moop.gamerguides.adapter.model.Videos
 import com.moop.gamerguides.helper.FirebaseUtil
+import com.moop.gamerguides.helper.ListToMutableList
 import com.squareup.picasso.Picasso
 
 class CourseDetails : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var firebaseDatabase: FirebaseDatabase
     private lateinit var firebaseStorage: FirebaseStorage
+    private lateinit var adapter: VideoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -76,9 +84,137 @@ class CourseDetails : AppCompatActivity() {
             }
 
         // get course videos from firebase database
+        // set adapter options
+        val options = FirebaseRecyclerOptions.Builder<Videos>()
+            .setQuery(firebaseDatabase.reference.child("courses").child(courseID).child("videos"), Videos::class.java)
+            .build()
 
+        // create adapter
+        adapter = VideoAdapter(options)
+        videoList.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+        videoList.adapter = adapter
 
-        // add new video button onclick function
+        // user profile card view onclick function
 
+        // add to favorite button onclick function
+        addToFavoriteButton.setOnClickListener {
+            // store user courses id
+            var userCourseList: List<String> = emptyList()
+            var addedCourseList: List<String?>
+            var removeCourseList: List<String?>
+
+            if (addToFavoriteButton.text == "Add to Favorite") {
+                // get all courses from favorites directory
+                firebaseDatabase.reference
+                    .child("users")
+                    .child(firebaseAuth.currentUser!!.uid)
+                    .child("favorites")
+                    .addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            val t = object : GenericTypeIndicator<List<String>>() {}
+                            if (snapshot.exists()) {
+                                // get courses in favorites directory
+                                userCourseList = snapshot.getValue(t)!!
+                            }
+                            // add a new course id into course list
+                            addedCourseList = ListToMutableList.addElement(userCourseList, courseID)
+
+                            // input new favorite course into the favorites directory in the user database
+                            firebaseDatabase.reference
+                                .child("users")
+                                .child(firebaseAuth.currentUser!!.uid)
+                                .child("favorites")
+                                .setValue(addedCourseList)
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {}
+
+                    })
+            }
+            else {
+                // get all courses from favorites directory
+                firebaseDatabase.reference
+                    .child("users")
+                    .child(firebaseAuth.currentUser!!.uid)
+                    .child("favorites")
+                    .addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            val t = object : GenericTypeIndicator<List<String>>() {}
+                            if (snapshot.exists()) {
+                                // get courses in favorites directory
+                                userCourseList = snapshot.getValue(t)!!
+                            }
+
+                            // remove a course from course list
+                            removeCourseList = ListToMutableList.removeElement(userCourseList, courseID)
+                            // input new favorite course into the favorites directory in the user database
+                            firebaseDatabase.reference
+                                .child("users")
+                                .child(firebaseAuth.currentUser!!.uid)
+                                .child("favorites")
+                                .setValue(removeCourseList)
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {}
+
+                    })
+            }
+        }
+
+        // change "Add to favorite" to "Remove from favorite"
+        checkIsFavorite(courseID, addToFavoriteButton)
+
+        // check is course favorite every 250 milliseconds
+        val refreshHandler = Handler()
+        val runnable: Runnable = object : Runnable {
+            override fun run() {
+                checkIsFavorite(courseID, addToFavoriteButton)
+                refreshHandler.postDelayed(this, 250)
+            }
+        }
+        refreshHandler.postDelayed(runnable, 250)
+    }
+
+    private fun checkIsFavorite(courseID: String, addToFavoriteButton: TextView) {
+        // store user courses id
+        var userCourseList: List<String> = emptyList()
+        // get all courses from favorites directory
+        firebaseDatabase.reference
+            .child("users")
+            .child(firebaseAuth.currentUser!!.uid)
+            .child("favorites")
+            .addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val t = object : GenericTypeIndicator<List<String>>() {}
+                    if (snapshot.exists()) {
+                        // get courses in favorites directory
+                        userCourseList = snapshot.getValue(t)!!
+                    }
+
+                    for (course in userCourseList) {
+                        if (course == courseID) {
+                            addToFavoriteButton.text = "Remove from Favorite"
+                            break
+                        }
+                        else {
+                            addToFavoriteButton.text = "Add to Favorite"
+                            break
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {}
+
+            })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
     }
 }
