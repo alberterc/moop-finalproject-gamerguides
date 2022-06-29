60c7409d7d58216603b8725cd5ac867ca48906e2
alberterc <85069947+alberterc@users.noreply.github.com>
2022-06-29 21:25:09 +0700

    Added video model to store video data.

--- app/src/main/java/com/moop/gamerguides/adapter/model/Video.kt
+++ app/src/main/java/com/moop/gamerguides/adapter/model/Video.kt
@@ -1,9 +1,21 @@
 package com.moop.gamerguides.adapter.model
 
-data class Video(
-    val id: String,
-    val title: String,
-    val description: String,
-    val thumbnailURL: String,
-    val videoURL: String
-)
+class Video {
+    var id: String = ""
+    var uid: String? = ""
+    var title: String = ""
+    var description: String = ""
+    var image: String = ""
+    var video: String = ""
+
+    constructor(title: String, image: String, description: String, video: String, uid: String, id: String) {
+        this.title = title
+        this.image = image
+        this.description = description
+        this.video = video
+        this.uid = uid
+        this.id = id
+    }
+
+    constructor() {} // needed for firebase
+}
