30cb80204529b79d528ea27c28e5915ecd7e8bf4
alberterc <85069947+alberterc@users.noreply.github.com>
2022-06-30 17:03:02 +0700

    Added util to delete a folder from firebase storage.

--- app/src/main/java/com/moop/gamerguides/helper/FirebaseUtil.kt
+++ app/src/main/java/com/moop/gamerguides/helper/FirebaseUtil.kt
@@ -1,6 +1,28 @@
 package com.moop.gamerguides.helper
 
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+
 object FirebaseUtil {
-    val firebaseDatabaseURL = "https://moop-group-finalproj-default-rtdb.asia-southeast1.firebasedatabase.app/"
-    val firebaseStorageURL = "gs://moop-group-finalproj.appspot.com"
+    const val firebaseDatabaseURL = "https://moop-group-finalproj-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    const val firebaseStorageURL = "gs://moop-group-finalproj.appspot.com"
+
+    fun deleteStorageFolder(path: String) {
+        // initialize Firebase storage
+        val firebaseStorage: FirebaseStorage = Firebase.storage(firebaseStorageURL)
+        val ref = firebaseStorage.reference.child(path)
+        ref.listAll()
+            .addOnSuccessListener { dir ->
+                dir.items.forEach { deleteStorageFile(ref.path, it.name) }
+                dir.prefixes.forEach { deleteStorageFolder(it.path) }
+            }
+    }
+
+    fun deleteStorageFile(path: String, name: String) {
+        // initialize Firebase storage
+        val firebaseStorage: FirebaseStorage = Firebase.storage(firebaseStorageURL)
+        val ref = firebaseStorage.reference.child(path).child(name)
+        ref.delete()
+    }
 }
