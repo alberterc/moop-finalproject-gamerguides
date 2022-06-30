7a9b17d1a6070216e89b901eba83f6fb6f8361dd
alberterc <85069947+alberterc@users.noreply.github.com>
2022-06-30 16:08:30 +0700

    Finished all onclick functions and added video list.

--- app/src/main/java/com/moop/gamerguides/MyCourseDetails.kt
+++ app/src/main/java/com/moop/gamerguides/MyCourseDetails.kt
@@ -1,11 +1,14 @@
 package com.moop.gamerguides
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.android.material.imageview.ShapeableImageView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -15,13 +18,17 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
+import com.moop.gamerguides.adapter.MyVideoAdapter
+import com.moop.gamerguides.adapter.model.Videos
 import com.moop.gamerguides.helper.FirebaseUtil
+import com.moop.gamerguides.helper.WrapContentLinearLayoutManager
 import com.squareup.picasso.Picasso
 
 class MyCourseDetails : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var firebaseDatabase: FirebaseDatabase
     private lateinit var firebaseStorage: FirebaseStorage
+    private lateinit var adapter: MyVideoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,9 +82,34 @@ class MyCourseDetails : AppCompatActivity() {
             }
 
         // get course videos from firebase database
+        // set adapter options
+        val options = FirebaseRecyclerOptions.Builder<Videos>()
+            .setQuery(firebaseDatabase.reference.child("courses").child(courseID).child("videos"), Videos::class.java)
+            .build()
 
+        // create adapter
+        adapter = MyVideoAdapter(options, courseID)
+        videoList.layoutManager = WrapContentLinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+        videoList.adapter = adapter
 
         // add new video button onclick function
+        addNewVideo.setOnClickListener {
+            val intent = Intent(applicationContext, AddNewVideo::class.java)
+            // get course id for intent
+            intent.putExtra("courseID", courseID)
+            // go to add new video activity with the course id
+            startActivity(intent)
+        }
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
     }
 }
