8e7a87cae1d75bbff8b0dbb818589b3111b7172e
alberterc <85069947+alberterc@users.noreply.github.com>
2022-06-30 17:57:47 +0700

    Added several new activities and temporarily change launcher activity.

---
+++ app/src/main/java/com/moop/gamerguides/UserProfileCourses.kt
@@ -0,0 +1,11 @@
+package com.moop.gamerguides
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserProfileCourses : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile_courses)
+    }
+}
---
+++ app/src/main/java/com/moop/gamerguides/VideoDetails.kt
@@ -0,0 +1,11 @@
+package com.moop.gamerguides
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VideoDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_details)
+    }
+}
